882ed025243cc8e586e6586f48d9f75e904268b7
Sree1701 <ksnavasree@gmail.com>
2026-09-11 14:33:51 +0530

    done

---
+++ scatter_sepal.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+iris=load_iris()
+sepal_length=iris.data[:,0]
+sepal_width=iris.data[:,1]
+plt.scatter(sepal_length,sepal_width,color='green',marker='o')
+plt.xlabel("Sepal Legth(cm)")
+plt.ylabel("Sepal Width(cm)")
+plt.title("Sepal Length vs Sepal Width(Iris Datasets)")
+plt.show()
